ed47e861582ce275e50de7d0c809f3b9bbaf6a4f
RUDY SANIEZ <20014077@M300004.hq.corp.leroymerlin.com>
2024-01-04 16:34:40 +0100

    + Clean

--- src/main/java/com/adeo/springboot/learning/sb3/mapper/VideoMapper.java
+++ src/main/java/com/adeo/springboot/learning/sb3/mapper/VideoMapper.java
@@ -2,10 +2,13 @@ package com.adeo.springboot.learning.sb3.mapper;
 
 import com.adeo.springboot.learning.sb3.domain.VideoEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper
 public interface VideoMapper {
 
+    @Mappings(value = @Mapping(target = "id", ignore = true))
     VideoEntity toEntity(com.adeo.springboot.learning.sb3.dto.Video video);
 
     com.adeo.springboot.learning.sb3.dto.Video toModel(VideoEntity video);
