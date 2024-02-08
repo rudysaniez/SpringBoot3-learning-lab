ee76b2cd8c67890a1e9fa97a187779b07e64182b
RUDY SANIEZ <20014077@M300004.hq.corp.leroymerlin.com>
2024-02-08 16:41:24 +0100

    + The manuals tests are complete

--- tests-manual-attributes-scenario-1-test.sh
+++ tests-manual-attributes-scenario-1-test.sh
@@ -17,7 +17,7 @@ BASE_PATH="v1/attributes"
 
 ####################
 
-testNumber="POST HTTP status [endpoint=$BASE_PATH] with [user:user]"
+testNumber="Bulk several attributes [endpoint=$BASE_PATH/:bulk] with [user:user]"
 result=$(eval getHttpStatusWithHttpMethod localhost $PORT POST "$BASE_PATH/:bulk" attributes.json user user)
 if [[ $result == "200" ]]
 then echo " > $testNumber : Http_status=$result."
@@ -27,7 +27,37 @@ fi
 
 ####################
 
-testNumber="GET HTTP status [endpoint=$BASE_PATH] with [user:user]"
+testNumber="Bulk async several attributes [endpoint=$BASE_PATH/:bulk-async] with [user:user]"
+result=$(eval getHttpStatusWithHttpMethod localhost $PORT POST "$BASE_PATH/:bulk-async" attributes.json user user)
+if [[ $result == "202" ]]
+then echo " > $testNumber : Http_status=$result."
+else
+  echo " > INCORRECT expected 202, we have $result"
+fi
+
+####################
+
+testNumber="Save one attribute [endpoint=$BASE_PATH] with [user:user]"
+result=$(eval getHttpStatusWithHttpMethod localhost $PORT POST "$BASE_PATH" attribute01.json user user)
+if [[ $result == "201" ]]
+then echo " > $testNumber : Http_status=$result."
+else
+  echo " > INCORRECT expected 201, we have $result"
+fi
+
+####################
+
+testNumber="Save async one attribute [endpoint=$BASE_PATH/:async] with [user:user]"
+result=$(eval getHttpStatusWithHttpMethod localhost $PORT POST "$BASE_PATH/:async" attribute02.json user user)
+if [[ $result == "202" ]]
+then echo " > $testNumber : Http_status=$result."
+else
+  echo " > INCORRECT expected 202, we have $result"
+fi
+
+####################
+
+testNumber="Get a page of attributes [endpoint=$BASE_PATH] with [user:user]"
 result=$(eval getHttpStatus localhost $PORT $BASE_PATH user user)
 if [[ $result == "200" ]]
 then echo " > $testNumber : Http_status=$result."
@@ -37,7 +67,7 @@ fi
 
 ####################
 
-testNumber="GET HTTP status [endpoint=$BASE_PATH/:search?q=code=code01] with [user:user]"
+testNumber="Search attributes [endpoint=$BASE_PATH/:search?q=code=code01] with [user:user]"
 result=$(eval getHttpStatus localhost $PORT "$BASE_PATH/:search?q=code=code01" user user)
 if [[ $result == "200" ]]
 then echo " > $testNumber : Http_status=$result."
@@ -47,7 +77,7 @@ fi
 
 ####################
 
-testNumber="GET HTTP status [endpoint=$BASE_PATH/:empty] with [user:user]"
+testNumber="Delete all attributes [endpoint=$BASE_PATH/:empty] with [user:user]"
 result=$(eval getHttpStatusWithHttpMethod localhost $PORT DELETE "$BASE_PATH/:empty" empty.json user user)
 if [[ $result == "200" ]]
 then echo " > $testNumber : Http_status=$result."
